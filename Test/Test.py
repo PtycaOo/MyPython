77e9397ea9a8295281a2f39baa128cf6a6701453
PtycaOo <DenKuzia5155@gmail.com>
2023-09-12 21:02:23 +0300

    №1

---
+++ Test/Test.py
@@ -0,0 +1,7 @@
+nums = [5, 7, 2, 4, 7, True, "Hello", 6.7,[5,6]] #prosto spisok []
+print(nums)
+
+nums[0] = True
+print(nums)
+print(nums[-1][1]) # Выводим последний элемент основного списка и последний элемент вложенного
+
---
+++ Test/Test_2.py
@@ -0,0 +1,8 @@
+numbers = [5,2,7]
+numbers.append(100) #append добавляет в список значения(в конец списка)
+print(numbers)
+
+numbers.insert(1, True) #вставляет значение по указанному индексу
+b = [5,9,2]
+numbers.extend(b) #добавляет в список ряд элементов
+numbers.sort()
