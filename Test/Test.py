29f0ab41605e26345742a76b0202e87eaaf97458
PtycaOo <Denkuzia5155@gmail.com>
2023-09-19 17:14:57 +0300

    123

--- Test/Test.py
+++ Test/Test.py
@@ -50,9 +50,3 @@
 
 # print(('a',1) in my_dict.items())   # True
 # print(('a',2) in my_dict.items())   # False
-
-my_dict = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
-print('a' in my_dict)               # True - без указания метода поиск по ключам
-print('a' in my_dict.keys())        # True - аналогично примеру выше
-print('a' in my_dict.values())      # False - так как 'а' — ключ, не значение
-print(1 in my_dict.values())        # True
